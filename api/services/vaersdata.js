4bed13d44e50476cc553098175544c3a42d2d988
Marvin Hoffenkamp <marvinhoffenkamp2001@gmail.com>
2021-03-30 15:38:21 +0200

    WIP api

---
+++ api/services/vaersdata.js
@@ -0,0 +1,23 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1){
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT VAERS_ID, RECVDATE, STATE, AGE_YRS, SEX, SYMPTOM_TEXT 
+    FROM 2021vaersdata LIMIT ?,?`, 
+    [offset, config.listPerPage]
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+module.exports = {
+  getMultiple
+}
