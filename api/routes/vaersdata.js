0abd79f455398f1bedaa0edc2ee60f2f305177fa
Marvin Hoffenkamp <marvinhoffenkamp2001@gmail.com>
2021-03-30 22:48:14 +0200

    add POST, PUT and DELETE endpoints

--- api/routes/vaersdata.js
+++ api/routes/vaersdata.js
@@ -12,4 +12,34 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+/* POST VAERSDATA */
+router.post('/', async function(req, res, next) {
+  try {
+    res.json(await vaersdata.create(req.body));
+  } catch (err) {
+    console.error(`Error while creating vaers entry`, err.message);
+    next(err);
+  }
+});
+
+/* PUT VAERSDATA */
+router.put('/:id', async function(req, res, next) {
+  try {
+    res.json(await vaersdata.update(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error while updating vaers entry`, err.message);
+    next(err);
+  }
+});
+
+/* DELETE VEARS entry */
+router.delete('/:id', async function(req, res, next) {
+  try {
+    res.json(await vaersdata.remove(req.params.id));
+  } catch (err) {
+    console.error(`Error while deleting vaers entry`, err.message);
+    next(err);
+  }
+});
+
 module.exports = router;
--- api/services/vaersdata.js
+++ api/services/vaersdata.js
@@ -7,7 +7,7 @@ async function getMultiple(page = 1){
   console.log("offset = " + offset)
   console.log("listperpage = " + config.listPerPage)
   const rows = await db.query(
-    `SELECT VAERS_ID, RECVDATE, STATE, AGE_YRS, SEX, SYMPTOM_TEXT 
+    `SELECT * 
     FROM 2021vaersdata LIMIT ? OFFSET ?`,
     [config.listPerPage, offset]
   );
@@ -20,6 +20,60 @@ async function getMultiple(page = 1){
   }
 }
 
+async function create(vaersdata){
+  const result = await db.query(
+    `INSERT INTO 2021vaersdata 
+    (VAERS_ID, RECVDATE, STATE, AGE_YRS, CAGE_YR, CAGE_MO, SEX, RPT_DATE, SYMPTOM_TEXT, DIED, DATEDIED, L_THREAT, ER_VISIT, HOSPITAL, HOSPDAYS, X_STAY, DISABLE, RECOVD, VAX_DATE, ONSET_DATE, NUMDAYS, LAB_DATA, V_ADMINBY, V_FUNDBY, OTHER_MEDS, CUR_ILL, HISTORY, PRIOR_VAX, SPLTTYPE, FORM_VERS, TODAYS_DATE, BIRTH_DEFECT, OFC_VISIT, ER_ED_VISIT, ALLERGIES) 
+    VALUES 
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
+    [vaersdata.VAERS_ID, vaersdata.RECVDATE, vaersdata.STATE, vaersdata.AGE_YRS, vaersdata.CAGE_YR, vaersdata.CAGE_MO, vaersdata.SEX, vaersdata.RPT_DATE, vaersdata.SYMPTOM_TEXT, vaersdata.DIED, vaersdata.DATEDIED, vaersdata.L_THREAT, vaersdata.ER_VISIT, vaersdata.HOSPITAL, vaersdata.HOSPDAYS, vaersdata.X_STAY, vaersdata.DISABLE, vaersdata.RECOVD, vaersdata.VAX_DATE, vaersdata.ONSET_DATE, vaersdata.NUMDAYS, vaersdata.LAB_DATA, vaersdata.V_ADMINBY, vaersdata.V_FUNDBY, vaersdata.OTHER_MEDS, vaersdata.CUR_ILL, vaersdata.HISTORY, vaersdata.PRIOR_VAX, vaersdata.SPLTTYPE, vaersdata.FORM_VERS, vaersdata.TODAYS_DATE, vaersdata.BIRTH_DEFECT, vaersdata.OFC_VISIT, vaersdata.ER_ED_VISIT, vaersdata.ALLERGIES]
+  );
+
+  let message = 'Error in creating data entry';
+
+  if (result.affectedRows) {
+    message = 'VAERS entry created successfully';
+  }
+
+  return {message};
+}
+
+async function update(entryID, vaersdata){
+  const result = await db.query(
+    `UPDATE 2021vaersdata 
+    SET RECVDATE=?, STATE=?, AGE_YRS=?, CAGE_YR=?, CAGE_MO=?, SEX=?, RPT_DATE=?, SYMPTOM_TEXT=?, DIED=?, DATEDIED=?, L_THREAT=?, ER_VISIT=?, HOSPITAL=?, HOSPDAYS=?, X_STAY=?, DISABLE=?, RECOVD=?, VAX_DATE=?, ONSET_DATE=?, NUMDAYS=?, LAB_DATA=?, V_ADMINBY=?, V_FUNDBY=?, OTHER_MEDS=?, CUR_ILL=?, HISTORY=?, PRIOR_VAX=?, SPLTTYPE=?, FORM_VERS=?, TODAYS_DATE=?, BIRTH_DEFECT=?, OFC_VISIT=?, ER_ED_VISIT=?, ALLERGIES=? 
+    WHERE VAERS_ID=?`, 
+    [vaersdata.RECVDATE, vaersdata.STATE, vaersdata.AGE_YRS, vaersdata.CAGE_YR, vaersdata.CAGE_MO, vaersdata.SEX, vaersdata.RPT_DATE, vaersdata.SYMPTOM_TEXT, vaersdata.DIED, vaersdata.DATEDIED, vaersdata.L_THREAT, vaersdata.ER_VISIT, vaersdata.HOSPITAL, vaersdata.HOSPDAYS, vaersdata.X_STAY, vaersdata.DISABLE, vaersdata.RECOVD, vaersdata.VAX_DATE, vaersdata.ONSET_DATE, vaersdata.NUMDAYS, vaersdata.LAB_DATA, vaersdata.V_ADMINBY, vaersdata.V_FUNDBY, vaersdata.OTHER_MEDS, vaersdata.CUR_ILL, vaersdata.HISTORY, vaersdata.PRIOR_VAX, vaersdata.SPLTTYPE, vaersdata.FORM_VERS, vaersdata.TODAYS_DATE, vaersdata.BIRTH_DEFECT, vaersdata.OFC_VISIT, vaersdata.ER_ED_VISIT, vaersdata.ALLERGIES, entryID]
+
+  );
+
+  let message = 'Error in updating VAERS entry';
+
+  if (result.affectedRows) {
+    message = 'VAERS entry updated successfully';
+  }
+
+  return {message};
+}
+
+async function remove(id){
+  const result = await db.query(
+    `DELETE FROM 2021vaersdata WHERE VAERS_ID=?`, 
+    [id]
+  );
+
+  let message = 'Error in deleting VAERS entry';
+
+  if (result.affectedRows) {
+    message = 'VAERS entry deleted successfully';
+  }
+
+  return {message};
+}
+
 module.exports = {
-  getMultiple
+  getMultiple,
+  create,
+  update,
+  remove
 }
